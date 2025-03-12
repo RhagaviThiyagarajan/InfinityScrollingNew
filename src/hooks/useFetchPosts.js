3c9ff485d4b0ad3112a3b6501d93f8e5ee89d982
Rhagavi Thiyagarajan <rhagavi3@gmail.com>
2025-03-12 11:45:45 +0530

    first commit

---
+++ src/hooks/useFetchPosts.js
@@ -0,0 +1,17 @@
+import { useInfiniteQuery } from "@tanstack/react-query";
+import axios from "axios";
+
+const fetchPosts = async ({ pageParam = 1 }) => {
+    const { data } = await axios.get(
+        `https://jsonplaceholder.typicode.com/posts?_page=${pageParam}&_limit=10`
+    );
+    return data;
+};
+
+export const useFetchPosts = () => {
+    return useInfiniteQuery({
+        queryKey: ["posts"],
+        queryFn: fetchPosts,
+        getNextPageParam: (lastPage, pages) => pages.length + 1, // Fetch next page
+    });
+};
